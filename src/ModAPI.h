60646267d40c964b186e2e870b11a6a094a069c7
Page3of9 <49576950+Page3of9@users.noreply.github.com>
2024-06-15 19:10:08 -0600

    Update ModAPI.h
    skse ----> SFSE

--- src/ModAPI.h
+++ src/ModAPI.h
@@ -38,28 +38,28 @@ namespace Messaging
 		}
 
 		// InterfaceVersion1
-		virtual APIResult AddPreHitCallback(SKSE::PluginHandle a_pluginHandle, PreHitCallback&& a_preHitCallback) noexcept override;
-		virtual APIResult AddPostHitCallback(SKSE::PluginHandle a_pluginHandle, PostHitCallback&& a_postHitCallback) noexcept override;
-		virtual APIResult AddPrePhysicsStepCallback(SKSE::PluginHandle a_pluginHandle, PrePhysicsStepCallback&& a_prePhysicsStepCallback) noexcept override;
-		virtual APIResult AddCollisionFilterComparisonCallback(SKSE::PluginHandle a_pluginHandle, CollisionFilterComparisonCallback&& a_collisionFilterComparisonCallback) noexcept override;
-		virtual APIResult RemovePreHitCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
-		virtual APIResult RemovePostHitCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
-		virtual APIResult RemovePrePhysicsStepCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
-		virtual APIResult RemoveCollisionFilterComparisonCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult AddPreHitCallback(SFSE::PluginHandle a_pluginHandle, PreHitCallback&& a_preHitCallback) noexcept override;
+		virtual APIResult AddPostHitCallback(SFSE::PluginHandle a_pluginHandle, PostHitCallback&& a_postHitCallback) noexcept override;
+		virtual APIResult AddPrePhysicsStepCallback(SFSE::PluginHandle a_pluginHandle, PrePhysicsStepCallback&& a_prePhysicsStepCallback) noexcept override;
+		virtual APIResult AddCollisionFilterComparisonCallback(SFSE::PluginHandle a_pluginHandle, CollisionFilterComparisonCallback&& a_collisionFilterComparisonCallback) noexcept override;
+		virtual APIResult RemovePreHitCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult RemovePostHitCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult RemovePrePhysicsStepCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult RemoveCollisionFilterComparisonCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
 		virtual float GetAttackCollisionCapsuleLength(RE::ActorHandle a_actorHandle, RequestedAttackCollisionType a_collisionType = RequestedAttackCollisionType::Default) const noexcept override;
 
 		// InterfaceVersion2
-		virtual APIResult AddWeaponWeaponCollisionCallback(SKSE::PluginHandle a_pluginHandle, WeaponCollisionCallback&& a_callback) noexcept override;
-		virtual APIResult RemoveWeaponWeaponCollisionCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
-		virtual APIResult AddWeaponProjectileCollisionCallback(SKSE::PluginHandle a_pluginHandle, WeaponCollisionCallback&& a_callback) noexcept override;
-		virtual APIResult RemoveWeaponProjectileCollisionCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult AddWeaponWeaponCollisionCallback(SFSE::PluginHandle a_pluginHandle, WeaponCollisionCallback&& a_callback) noexcept override;
+		virtual APIResult RemoveWeaponWeaponCollisionCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult AddWeaponProjectileCollisionCallback(SFSE::PluginHandle a_pluginHandle, WeaponCollisionCallback&& a_callback) noexcept override;
+		virtual APIResult RemoveWeaponProjectileCollisionCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
 		virtual void ApplyHitImpulse(RE::ActorHandle a_actorHandle, RE::hkpRigidBody* a_rigidBody, const RE::NiPoint3& a_hitVelocity, const RE::hkVector4& a_hitPosition, float a_impulseMult) noexcept override;
 
 		// InterfaceVersion3
-		virtual APIResult AddCollisionFilterSetupCallback(SKSE::PluginHandle a_pluginHandle, CollisionFilterSetupCallback&& a_callback) noexcept override;
-		virtual APIResult RemoveCollisionFilterSetupCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
-		virtual APIResult AddContactListenerCallback(SKSE::PluginHandle a_pluginHandle, ContactListenerCallback&& a_callback) noexcept override;
-		virtual APIResult RemoveContactListenerCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult AddCollisionFilterSetupCallback(SFSE::PluginHandle a_pluginHandle, CollisionFilterSetupCallback&& a_callback) noexcept override;
+		virtual APIResult RemoveCollisionFilterSetupCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult AddContactListenerCallback(SFSE::PluginHandle a_pluginHandle, ContactListenerCallback&& a_callback) noexcept override;
+		virtual APIResult RemoveContactListenerCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
 		virtual bool IsActorActive(RE::ActorHandle a_actorHandle) const noexcept override;
 		virtual bool IsActorActiveCollisionGroup(uint16_t a_collisionGroup) const noexcept override;
 		virtual bool IsActorCharacterControllerHittable(RE::ActorHandle a_actorHandle) const noexcept override;
@@ -68,8 +68,8 @@ namespace Messaging
 		virtual bool ToggleDisableActor(RE::ActorHandle a_actorHandle, bool a_bDisable) noexcept override;
 
 		// InterfaceVersion4
-		virtual APIResult AddPrecisionLayerSetupCallback(SKSE::PluginHandle a_pluginHandle, PrecisionLayerSetupCallback&& a_callback) noexcept override;
-		virtual APIResult RemovePrecisionLayerSetupCallback(SKSE::PluginHandle a_pluginHandle) noexcept override;
+		virtual APIResult AddPrecisionLayerSetupCallback(SFSE::PluginHandle a_pluginHandle, PrecisionLayerSetupCallback&& a_callback) noexcept override;
+		virtual APIResult RemovePrecisionLayerSetupCallback(SFSE::PluginHandle a_pluginHandle) noexcept override;
 		RE::NiAVObject* GetOriginalFromClone(RE::ActorHandle a_actorHandle, RE::NiAVObject* a_node) noexcept override;
 		RE::hkpRigidBody* GetOriginalFromClone(RE::ActorHandle a_actorHandle, RE::hkpRigidBody* a_hkpRigidBody) noexcept override;
 		virtual void ApplyHitImpulse2(RE::ActorHandle a_targetActorHandle, RE::ActorHandle a_sourceActorHandle, RE::hkpRigidBody* a_rigidBody, const RE::NiPoint3& a_hitVelocity, const RE::hkVector4& a_hitPosition, float a_impulseMult) noexcept override;
